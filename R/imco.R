9426842b7fde5da0d8dc4f5ba745b6c337f6ea39
lindaxu52 <linda.b.xu@gmail.com>
2024-03-18 23:56:01 -0400

    Update imco.R

--- R/imco.R
+++ R/imco.R
@@ -38,9 +38,9 @@ imco <- function(files, brain_mask,
     if (!all(dim(fileList[[i - 1]]) == dim(fileList[[i]]))) {
       stop("Image dimensions do not match")
     }
-    if (!all(ANTsRCore::antsGetSpacing(fileList[[i - 1]]) == ANTsRCore::antsGetSpacing(fileList[[i]]))) {
-      stop("Voxel dimensions do not match")
-    }
+    #if (!all(ANTsRCore::antsGetSpacing(fileList[[i - 1]]) == ANTsRCore::antsGetSpacing(fileList[[i]]))) {
+    #  stop("Voxel dimensions do not match")
+    #}
     #if (!all(ANTsRCore::antsGetDirection(fileList[[i - 1]]) == ANTsRCore::antsGetDirection(fileList[[i]]))) {
     #  stop("Image directions do not match")
     #}
