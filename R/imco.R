46406b4cfba424ef0184ed2be1ab8d284e73198e
lindaxu52 <linda.b.xu@gmail.com>
2024-08-11 00:59:49 -0400

    Update imco.R

--- R/imco.R
+++ R/imco.R
@@ -53,9 +53,9 @@ imco <- function(files, brain_mask,
   if (!all(dim(mask) == dim(fileList[[1]]))) {
     stop("Image dimensions do not match the brain mask")
   }
-  #if (!all(ANTsRCore::antsGetSpacing(mask) == ANTsRCore::antsGetSpacing(fileList[[1]]))) {
-  #  stop("Voxel dimensions do not match the brain mask")
-  #}
+  if (!all(ANTsRCore::antsGetSpacing(mask) == ANTsRCore::antsGetSpacing(fileList[[1]]))) {
+    stop("Voxel dimensions do not match the brain mask")
+  }
 
   if (!is.numeric(fwhm)) {
     fwhm <- as.numeric(fwhm)
@@ -81,8 +81,8 @@ imco <- function(files, brain_mask,
   fileList <- lapply(fileList, check_ants)
 
   # Dimension of each voxel in mm
-  #vDims <- ANTsRCore::antsGetSpacing(fileList[[1]])
-  vDims <- ANTsRCore::antsImage_GetSpacing(fileList[[1]])
+  vDims <- ANTsRCore::antsGetSpacing(fileList[[1]])
+  #vDims <- ANTsRCore::antsImage_GetSpacing(fileList[[1]])
 
   nhood_dims <- get_nhood_size(
     fwhm = fwhm,
